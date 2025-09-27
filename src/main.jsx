2a58a881f8e5333ed3802de1955244b615e71893
jbriones2901-art <jbriones.2901@gmail.com>
2025-09-27 11:44:48 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import KumoPage from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <KumoPage />
+  </React.StrictMode>
+);
+
 
